dad752c5cb3117ac7b438b65db7a90bafd63770a
Daniel Herrera <daniel.herrera2@teradata.com>
2024-12-11 12:08:41 +0100

    usually the target folder is not tracked

--- target/run/jaffle_shop_teradata/models/discovery/light-integration/lim_customers.sql
+++
@@ -1,6 +0,0 @@
-
-   
-    INSERT INTO "demo_user"."lim_customers" ("customer_id", "first_name", "email", "valid_period", "customer_key", "family_key")
-       SELECT "customer_id", "first_name", "email", "valid_period", "customer_key", "family_key"
-       FROM "demo_user"."lim_customers__dbt_tmp"
-    ;
--- target/run/jaffle_shop_teradata/models/discovery/light-integration/lim_orders.sql
+++
@@ -1,6 +0,0 @@
-
-   
-    INSERT INTO "demo_user"."lim_orders" ("id", "user_id", "order_date", "status", "last_update_ts", "email", "customer_key")
-       SELECT "id", "user_id", "order_date", "status", "last_update_ts", "email", "customer_key"
-       FROM "demo_user"."lim_orders__dbt_tmp"
-    ;
